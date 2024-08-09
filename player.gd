4864a056c0da69ec48b8eb1626cd2cd564b1f054
Luiz G. <luiz007@live.com>
2024-08-09 18:10:43 -0300

    Update controls to work with mobile

--- player.gd
+++ player.gd
@@ -5,20 +5,43 @@ extends CharacterBody2D
 
 var sync_position: Vector2
 
+var target_pos: Vector2
+var clicking := false
+
 func _ready():
     sync_position = position
     multiplayer_sync.set_multiplayer_authority(name.to_int())
+    target_pos = position
+
+func _input(event):
+    if event is InputEventScreenTouch:
+        if event.pressed:
+            target_pos = event.position
+            clicking = true
+        else:
+            target_pos = position
+            clicking = false
+
+func get_input():
+    #var input_direction = Input.get_vector("left", "right", "up", "down")
+    #velocity = input_direction * speed
+    clicking = Input.is_action_pressed("click")
+    if clicking:
+        target_pos = get_global_mouse_position()
+    else:
+        target_pos = global_position
     
+    position = target_pos
+     
+
 func _physics_process(delta):
     if not multiplayer_sync.get_multiplayer_authority() == multiplayer.get_unique_id():
         position = position.lerp(sync_position, 0.5)
-        
     else:
-        var input_direction = Input.get_vector("left", "right", "up", "down")
-        velocity = input_direction * speed
+        get_input()
         var collision = move_and_collide(velocity * delta)
         if collision:
-            velocity = velocity.slide(collision.get_normal())
+            #velocity = velocity.slide(collision.get_normal())
             var collider = collision.get_collider()
             if collider.has_method("apply_impulse"):
                 var push_to_dir = (collider.global_position - global_position).normalized()
